8596ce868e33e4e96fac8187f3adb492a0bdfc5c
Jinha Kim <goldbar80@gmail.com>
2013-07-10 12:37:33 +0900

    temporary workaround for org 8.0

--- init.el
+++ init.el
@@ -5,14 +5,16 @@
 ;; This is the first thing to get loaded.
 ;;
 
+;; temporary workaround for org 8.0  -- install org before org-mode is loaded
+;;  (package-install 'org))           ;; installed, install it if not
+
 ;; load Org-mode from source when the ORG_HOME environment variable is set
 (when (getenv "ORG_HOME")
   (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
     (when (file-directory-p org-lisp-dir)
       (add-to-list 'load-path org-lisp-dir)
       (require 'org))))
-
-;; load the starter kit from the `after-init-hook' so all packages are loaded
+ ;; load the starter kit from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
  `(lambda ()
     ;; remember this directory
